aebc384e483472106483742bd66292e9f0bfdd5c
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-05-21 18:43:10 +0100

    bug in put player move

--- minimax.cpp
+++ minimax.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 const char PLAYER_1 = 'X' ;
@@ -54,10 +55,10 @@ int calcule_score_board(board_vec board)
     return 0;
 }
 
-
 int minimax(board_vec board, int dept, char player)
 {
     int score = calcule_score_board(board);
+    int bets_score;
     if (score != 0)
     {
         if (score == WIN)
@@ -73,38 +74,52 @@ int minimax(board_vec board, int dept, char player)
                 if (board[i][j] == ' ')
                 {
                     board[i][j] = player;
-                    score = minimax(board, dept + 1, PLAYER_1);
+                    if (player == PLAYER_1)
+                    {
+                        bets_score = -100;
+                        score = minimax(board, dept + 1, PLAYER_2);
+                        bets_score = max(score, bets_score);
+                    }
+                    else
+                    {
+                        bets_score = 100;
+                        score = minimax(board, dept + 1, PLAYER_1);
+                        bets_score = min(score, bets_score);
+                    }
                     board[i][j] = ' ';
                 }
             }
         }
     }
-    return 1;
+    return (bets_score);
 }
 
-vector <s_board>  get_the_best_move(board_vec board, char player)
+int   *get_the_best_move(board_vec board, char player)
 {
-    vector<s_board> all_possible;
+    int best_score = -100;
+    int score;
+    int *move = new int[2];
 
     for (size_t i = 0; i < 3; i++)
     {
         for (size_t j = 0; j < 3; j++)
         {
-            s_board board_cas;
             if (board[i][j] == ' ')
             {
                 board[i][j] = player;
-                board_cas.board = board;
-                board_cas.score = minimax(board_cas.board, 1, PLAYER_1);
+                score = minimax(board, 1, player);
+                if (score > best_score)
+                {
+                    best_score = score;
+                    move[0] = i;
+                    move[1] = j;
+                }
                 board[i][j] = ' ';
-                all_possible.push_back(board_cas);
             }
         }
-        
     }
-    return all_possible;
+    return (move);
 }
-
 
 board_vec initial_board()
 {
@@ -121,10 +136,48 @@ board_vec initial_board()
     return board;
 }
 
+void    print_board(board_vec board)
+{
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            printf("%c |", board[i][j]);
+        }
+        printf("\n---------\n");
+    }
+}
 
+int game_still_running(board_vec board)
+{
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+           if (board[i][j] == ' ') 
+                return 1;
+        }
+        
+    }
+    return 0;
+}
 int main(int argc, char const *argv[])
 {
     board_vec board;
     board = initial_board();
+    int *best_move;
+    int x, y, p_x, p_y;
+    while (game_still_running(board))
+    {
+        best_move = get_the_best_move(board, PLAYER_1);
+        y = best_move[0];
+        x = best_move[1];
+        board[y][x] = PLAYER_1;
+        print_board(board);
+        cin >> p_y >> p_x;
+        board[p_y][p_x] = PLAYER_2;
+        print_board(board);
+    }
+    
     return 0;
 }
