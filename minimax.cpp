b7fa4f7b8a644364e6317157fdaf17aeb9a5ee51
benlyazid <benlyazidbest@gmail.com>
2021-05-22 23:08:50 +0100

    finish the algo

--- minimax.cpp
+++ minimax.cpp
@@ -32,34 +32,34 @@ int calcule_score_board(board_vec board)
         {
             //printf("check11\n");
             if (board[i][0] == PLAYER_1)
-                return 10;
+                return WIN;
             else if (board[i][0] == PLAYER_2)
-                return -10;
+                return LOSE;
         } 
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
         {
             //printf("check12 %ld\n", i);
             if (board[0][i] == PLAYER_1)
-                return 10;
+                return WIN;
             if (board[0][i] == PLAYER_2)
-                return -10;
+                return LOSE;
         }
     }
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
     {
         if (board[0][0] == PLAYER_1)
-            return 10;
+            return WIN;
         if (board[0][0] == PLAYER_2)
-            return -10;
+            return LOSE;
     } 
     if (board[2][0] == board[1][1] && board[1][1] == board[0][2])
     {
         if (board[2][0] == PLAYER_1)
-            return 10;
+            return WIN;
         if (board[2][0] == PLAYER_2)
-            return -10;
+            return LOSE;
     }
-    return 0;
+    return TIE;
 }
 
 
@@ -72,45 +72,71 @@ int minimax(board_vec board, int dept, char player, bool print)
     //print_board(board);
     //printf("check score is %d   %d last paly by %c \n", score, dept, player);
 
-    if (score != 0)
+    if (score == WIN)
     {
-        if (score == WIN)
-        {
-           // printf("win score is %d\n", 1);
-
-            return WIN;
-        }
+        //print_board(board);
+       // printf("win score is %d\n", 1);
+        return WIN;
+    }
+    if (score == LOSE)
+    {
+        //print_board(board);
         //printf("lose score is %d\n", -1);
-
         return LOSE;
     }
+
     if ((game_still_running(board)) == 0)
-        return TIE;
-    for (size_t i = 0; i < 3; i++)
     {
-        for (size_t j = 0; j < 3; j++)
+        
+       // print_board(board);
+       // printf("TIe score is %d\n", 0);
+        return TIE;
+    }
+    if (player == PLAYER_1)
+    {
+
+        best_score = -100;
+        for (size_t i = 0; i < 3; i++)
         {
-            if (board[i][j] == ' ')
+            for (size_t j = 0; j < 3; j++)
             {
-                board[i][j] = player;
-                printf("palyer is %c    %d\n", player, dept);
-                if (player == PLAYER_1)
+                if (board[i][j] == ' ')
                 {
-                    best_score = -100;
+                    board[i][j] = player;
+                    //printf("palyer is %c    %d\n", player, dept);
                     score = minimax(board, dept + 1, PLAYER_2, print);
                     best_score = max(score, best_score);
+                    //print_board(board);
+                    //printf("return for the top board %d in dept %d\n", score, dept);
+                    board[i][j] = ' ';
                 }
-                else
-                {
-                    best_score = 100;
-                    score = minimax(board, dept + 1, PLAYER_1, print);
-                    //printf("min score is %d %d\n", score, best_score);
-                    best_score = min(score, best_score);
-                }
-                board[i][j] = ' ';
             }
         }
     }
+    if (player == PLAYER_2)
+    {
+
+        best_score = 100;
+        for (size_t i = 0; i < 3; i++)
+        {
+            for (size_t j = 0; j < 3; j++)
+            {
+                if (board[i][j] == ' ')
+                {
+                    board[i][j] = player;
+                    //printf("palyer is %c    %d\n", player, dept);
+                    score = minimax(board, dept + 1, PLAYER_1, print);
+                    //printf("min score is %d %d\n", score, best_score);
+                    best_score = min(score, best_score);
+                   // print_board(board);
+                    //printf("return for the top board %d in dept %d\n", score, dept);
+                    board[i][j] = ' ';
+                }
+            }
+        }
+    }
+    //print_board(board);
+    //printf("return for the top board %d in dept %d\n", best_score, dept);
     return (best_score);
 }
 
@@ -127,16 +153,17 @@ int   *get_the_best_move(board_vec board, char player)
         {
             if (board[i][j] == ' ')
             {
-                //board[i][j] = player;
-                score = minimax(board, 1, player, true);
-                printf("%d  for %ld %ld\n", score, i, j);
+                board[i][j] = PLAYER_1;
+                score = minimax(board, 1, PLAYER_2, false);
+                //printf("%d  for %ld %ld\n", score, i, j);
+                board[i][j] = ' ';
+
                 if (score > best_score)
                 {
                     best_score = score;
                     move[0] = i;
                     move[1] = j;
                 }
-                //board[i][j] = ' ';
             }
         }
     }
@@ -173,6 +200,8 @@ void    print_board(board_vec board)
 
 int game_still_running(board_vec board)
 {
+    if (calcule_score_board(board) != 0)
+        return 0;
     for (size_t i = 0; i < 3; i++)
     {
         for (size_t j = 0; j < 3; j++)
@@ -180,7 +209,6 @@ int game_still_running(board_vec board)
            if (board[i][j] == ' ') 
                 return 1;
         }
-        
     }
     return 0;
 }
@@ -188,12 +216,13 @@ int main(int argc, char const *argv[])
 {
     board_vec board;
     board = initial_board();
-    board[0][0] = 'O';
-    board[0][2] = 'X';
+    /*board[0][0] = 'X';
+    board[0][1] = 'O';
+    board[0][2] = 'O';
     board[1][1] = 'O';
-    board[1][2] = 'O';
+    board[2][1] = 'X';
     board[2][2] = 'X';
-    print_board(board);
+    print_board(board);*/
     int *best_move;
 
     int x, y, p_x, p_y;
@@ -205,6 +234,8 @@ int main(int argc, char const *argv[])
         x = best_move[1];
         board[y][x] = PLAYER_1;
         print_board(board);
+        if (game_still_running(board) == 0)
+            break;
         cin >> p_y >> p_x;
         board[p_y][p_x] = PLAYER_2;
         print_board(board);
